dbff637b687c6b3b22be89d3c24a2976bcc13e6c
DARKXSIDE78 <nk1010057@gmail.com>
2025-06-30 11:48:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+API_ID = "" #API ID get it from telegram.org
+API_HASH = "" #API HASH get it from telegram.org
+BOT_TOKEN = "" #Bot Token get it from @BotFather
+URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
+#URL_B = "" #News RSS Feed Url 2
+START_PIC = "https://images6.alphacoders.com/131/1313754.png" #Startup Pic
+MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
+ADMINS = [7086472788]
