87e39e24b915d45893446a1da6edf6913d160b1f
mikotest4 <fakealok756@gmail.com>
2025-07-19 00:05:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "" #API ID get it from telegram.org
-API_HASH = "" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+API_ID = "27704224" #API ID get it from telegram.org
+API_HASH = "c2e33826d757fe113bc154fcfabc987d" #API HASH get it from telegram.org
+BOT_TOKEN = "7819249411:AAEruuIp0IYeS5dOClQvRXBHRlgeC_-mFeE" #Bot Token get it from @BotFather
 URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "https://images6.alphacoders.com/131/1313754.png" #Startup Pic [Optional]
-MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
-ADMINS = [7086472788]
+MONGO_URI = "mongodb+srv://Koi:aloksingh@cluster0.86wo9.mongodb.net/?retryWrites=true&w=majority" #Databse uri get it from https://www.mongodb.com/
+ADMINS = [7970350353]
